60cc94d7aec2eb890e7204b812244f9c0b30fdd2
rayphay <rayphay4real@gmail.com>
2026-02-19 13:01:38 +0100

    my app visuals

--- script.js
+++ script.js
@@ -1,103 +1,90 @@
-console.log("Hello World!, welcome to JS Review"); //for printing the info in the quotation marks
-
-//alert("Welcome to JS Review")  //for notification pop-ups
-
-const firstname = "Raphael";
-const lastname = "Oresanwo";
-let val = 34;
-const fullname = firstname+" "+lastname;
-console.log("My fullname is "+fullname);
-
-//Array Declaration
-const arr = [
-    "Raphael",
-    34,
-    true,
-    null
-]
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[2]);
 
 
-const x = 42;
-const explicit = String(x);
-const implicit = x + "";
-console.log("Explicit type: "+typeof(explicit));
-console.log("Implicit type: "+typeof(implicit));
-
-//we can also compare two values using the comparison operator
-const a = 42;
-const b = 22;
-console.log(a == b);
-console.log(a <= b);
-let test = (a === '42');
-console.log(test);
-
-let age = 42;
-let canDrive = age > 16 ? 'yes' : 'no';
-console.log("Can I drive? "+canDrive);
-
-let minimumAge = 18;
-let myAge = 42;
-// Check if the person is eligible to vote based on their age
-if(myAge < minimumAge) {
-    console.log("You are not eligible to vote")
-}
-else {
-    console.log("Please do go ahead and cast your vote")
+/**
+ * Get the car data, map to only the variables 
+ * we are interested in
+ * and clean any missing data
+ */
+async function loadData() { // async is for asynchronous code, 
+// it allows us to use the await keyword inside the function
+    const carsDataResponse = await fetch 
+    ("https://storage.googleapis.com/tfjs-tutorials/carsData.json");
+    const carsData = await carsDataResponse.json();
+    const cleaned = carsData.map(car=>({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+    }))
+    .filter(car=> (car.mpg != null && car.horsepower != null));
+    return cleaned;
 }
 
-let quartile = 50;
-// Determine the quartile based on the value of 'quartile' variable
-switch(quartile) {
-    case 25:
-        console.log("First quartile");
-        break;
-    case 50:
-        console.log("Second quartile");
-        break;
-    case 75:
-        console.log("3rd Quartile");
-        break;
-    default:
-        console.log("You are in the 4th quartile")
+async function run() {
+    // load and plot the original input data we are going to train on
+    const data = await loadData();
+    const values = data.map(d=>({
+        x: d.horsepower,
+        y: d.mpg,
+    }));
+    // Scatter plot of horsepower vs MPG
+    tfvis.render.scatterplot(
+        {name: 'Scatter plot of Horsepower vs MPG'},
+        {values},
+        {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 300
+        }
+    );
+    //create the model
+    const model = createModel();
+    tfvis.show.modelSummary({name: 'Model Summary'}, model);
+    const tensorData =  convertToTensor(data);
+    const {inputs, labels} = tensorData;
+
+    //Train the model
+    //await trainModel(model, inputs, labels);
+    //console.log('Done Training!!!');
+
+    // Make some predictions using the model and compare them to the original data
+    //testModel(model, data, tensorData);
+     
+    // Prepare the data and convert to appropriate 
+    //format for barchart
+    const barchartValues = data.map((d, i) =>({
+        index: i,
+        value: d.mpg,
+    }));
+    
+    //Show a barchart of the MPG values
+    tfvis.render.barchart(
+        {name: 'Bar Chart of the MPG values'},
+        barchartValues, {height: 300, width: 800, 
+            fontSize:16,xLabel:"Index", yLabel:"MPG"}, 
+            series=['mpg']
+    )
+    //Show a line chart of the MPG values
+    tfvis.render.linechart(
+        {name: 'Sample Line Chart'},
+        {values,series: ['mpg']},
+        {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 1000,
+            width: 800,
+        }
+    );
+    // Prepare the data and convert to appropriate 
+    // format for histogram
+    const histData = data
+    .map(d => d.horsepower);
+    console.log("histData max=="+Math.max(...histData))
+    console.log("histData min=="+Math.min(...histData))
+    tfvis.render.histogram({name:"Histogram of the car's MPG"},
+        histData,{
+            maxBin:20,
+            height:450,
+            fontSize:16
+        })
 }
 
-// For loop
-// Loop through numbers 0 to 4 and print the index
-for( let i = 0; i < 5; i++){
-        console.log("My index is "+i)
-}
-
-// functions- by declaration
-
-function addTwoNumbers(a, b) {
-    return a + b;
-}
-
-// function by expression
-const addtwoNumbers = function(a, b) {
-    return a + b;
-}
-console.log(addTwoNumbers(2,3));
-console.log(addtwoNumbers(2,4));
-
-const sum = (a,b) => a + b;
-const log = (output) => console.log(output);
-let result = sum(2,8)  //gives ?
-log("My sum function result is::"+result);  //My sum function is  ?
-
-// Template Literals
-const name = "Afam Okonkwo";
-age = 30;
-const message = `My name is ${name} and I am ${age} years old.`;
-console.log(message);
-console.log(`my sum is ${sum(2,3)}`);
-
-
-
-const courseTopic = "ES 6 Review"
-const courseCode = "PAU-ISM 210"
-
-console.log(`Hi, welcome to ${courseTopic}, you enrolled for ${courseCode}`);
+document.addEventListener('DOMContentLoaded', run);
